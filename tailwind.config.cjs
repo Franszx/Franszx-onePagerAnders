8d4f470fa060278ea7aedb7fbcdd088e902942ae
Anders Lindberg <anderslindberg999@gmail.com>
2023-11-22 13:30:45 +0100

    svg update

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,14 +2,15 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 const colors = require("tailwindcss/colors");
 module.exports = {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
-
       colors: {
         //white: "#8f9197",
-
-        secondary: "#0000ff",
+        five: "#AE445A",
+        four: "#451952",
+        third: "#662549",
+        secondary: "#F39F5A",
         primary: "#fafafa",
       },
       fontFamily: {
@@ -17,5 +18,4 @@ module.exports = {
       },
     },
   },
-
-}
+};
